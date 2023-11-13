688be1cf57b1b1c23363d05427edca202afd7866
dmitry131131 <antontsev.dmitry@yandex.ru>
2023-11-13 18:45:30 +0300

    Made base structure of project

---
+++ include/Tree.h
@@ -0,0 +1,9 @@
+/**
+ * @file
+ * @brief Main tree functions
+*/
+#ifndef TREE_H
+#define TREE_H
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "Tree.h"
+
+int main()
+{
+
+    return 0;
+}
